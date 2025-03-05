5e23d7765f72ccc523ec0fa3a5c8af7bd1614f5f
Dairo Mosquera <dairoandres115@outlook.com>
2025-03-04 21:02:03 -0500

    test: update testing file

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
-from odata_v4_query import ODataQueryParser
+from odata_v4_query import ODataQueryParser, ODataFilterParser
 
+# Create parser instance
 parser = ODataQueryParser()
 
-print(parser.parse_url('https://example.com/odata?$count=true&$top=10&$skip=20'))
+# Parse a complete URL
+options = parser.parse_url('https://example.com/odata?$count=true&$top=10&$skip=20')
+print(options)
+
+# Parse just the query string
+options = parser.parse_query_string('$filter=name eq \'John\' and age gt 25')
+print(options)
+
+# Parse filter expressions
+filter_parser = ODataFilterParser()
+ast = filter_parser.parse('name eq \'John\' and age gt 25')
+print(ast)
