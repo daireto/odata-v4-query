8ef0f062e7ca39452a1500a352bd937cb0396888
Dairo Mosquera <dairoandres115@outlook.com>
2025-03-04 22:10:50 -0500

    feat: implement protocols and refactor code

--- odata_v4_query/query_parser.py
+++ odata_v4_query/query_parser.py
@@ -1,9 +1,15 @@
+from collections.abc import Callable, Sequence
 from dataclasses import dataclass
-from typing import Literal
 from urllib.parse import parse_qs, urlparse
 
 from .errors import NoPositiveIntegerValue, UnsupportedFormat
-from .filter_parser import FilterNode, ODataFilterParser
+from .filter_parser import (
+    FilterNode,
+    ODataFilterParser,
+    ODataFilterParserProtocol,
+)
+
+OptionCallback = Callable[[str, 'ODataQueryOptions'], None]
 
 
 @dataclass
@@ -11,7 +17,7 @@ class ODataQueryOptions:
     count: bool = False
     expand: list[str] | None = None
     filter_: FilterNode | None = None
-    format_: Literal['json', 'xml', 'csv', 'tsv'] | None = None
+    format_: str | None = None
     orderby: list[dict[str, str]] | None = None
     search: str | None = None
     select: list[str] | None = None
@@ -24,19 +30,91 @@ class ODataQueryParser:
     standard query parameters.
     """
 
-    def __init__(self):
-        self.supported_options = {
-            '$count': self._parse_count,
-            '$expand': self._parse_expand,
-            '$filter': self._parse_filter,
-            '$format': self._parse_format,
-            '$orderby': self._parse_orderby,
-            '$search': self._parse_search,
-            '$select': self._parse_select,
-            '$skip': self._parse_skip,
-            '$top': self._parse_top,
-        }
-        self.filter_parser = ODataFilterParser()
+    __supported_options: dict[str, OptionCallback]
+    """Supported query options."""
+
+    __supported_formats: Sequence[str]
+    """Supported response formats."""
+
+    __filter_parser: ODataFilterParserProtocol
+    """Filter parser."""
+
+    def __init__(
+        self,
+        supported_options: dict[str, OptionCallback] | None = None,
+        supported_formats: Sequence[str] | None = None,
+        filter_parser: ODataFilterParserProtocol | None = None,
+    ) -> None:
+        """Parser for OData V4 query options supporting
+        standard query parameters.
+
+        Parameters
+        ----------
+        supported_options : dict[str, OptionCallback] | None, optional
+            Dictionary of supported query options, by default None.
+        supported_formats : Sequence[str] | None, optional
+            Sequence of supported response formats, by default None.
+        filter_parser : ODataFilterParserProtocol | None, optional
+            Filter parser, by default None.
+        """
+        if supported_options is not None:
+            self.__supported_options = supported_options
+        else:
+            self.__supported_options = {
+                '$count': self._parse_count,
+                '$expand': self._parse_expand,
+                '$filter': self._parse_filter,
+                '$format': self._parse_format,
+                '$orderby': self._parse_orderby,
+                '$search': self._parse_search,
+                '$select': self._parse_select,
+                '$skip': self._parse_skip,
+                '$top': self._parse_top,
+            }
+
+        if supported_formats is not None:
+            self.__supported_formats = supported_formats
+        else:
+            self.__supported_formats = ('json', 'xml', 'csv', 'tsv')
+
+        if filter_parser is not None:
+            self.__filter_parser = filter_parser
+        else:
+            self.__filter_parser = ODataFilterParser()
+
+    def set_supported_options(
+        self, supported_options: dict[str, OptionCallback]
+    ) -> None:
+        """Sets the supported query options.
+
+        Parameters
+        ----------
+        supported_options : dict[str, OptionCallback]
+            Dictionary of supported query options.
+        """
+        self.__supported_options = supported_options
+
+    def set_supported_formats(self, supported_formats: Sequence[str]) -> None:
+        """Sets the supported response formats.
+
+        Parameters
+        ----------
+        supported_formats : Sequence[str]
+            Sequence of supported response formats.
+        """
+        self.__supported_formats = supported_formats
+
+    def set_filter_parser(
+        self, filter_parser: ODataFilterParserProtocol
+    ) -> None:
+        """Sets the filter parser.
+
+        Parameters
+        ----------
+        filter_parser : ODataFilterParserProtocol
+            Filter parser.
+        """
+        self.__filter_parser = filter_parser
 
     def parse_url(self, url: str) -> ODataQueryOptions:
         """Parses a complete OData URL and
@@ -91,14 +169,14 @@ class ODataQueryParser:
         options = ODataQueryOptions()
 
         for param, values in query_params.items():
-            if param in self.supported_options:
+            if param in self.__supported_options:
                 # get the first value since OData parameters shouldn't
                 # have multiple values
                 value = values[0] if values else None
-                if value is None:
+                if not value:
                     continue
 
-                parser_func = self.supported_options[param]
+                parser_func = self.__supported_options[param]
                 parser_func(value, options)
 
         return options
@@ -125,8 +203,7 @@ class ODataQueryParser:
         options : ODataQueryOptions
             Current query options object.
         """
-        if value:
-            options.expand = [item.strip() for item in value.split(',')]
+        options.expand = self._split_by_comma(value)
 
     def _parse_filter(self, value: str, options: ODataQueryOptions) -> None:
         """Parses $filter parameter.
@@ -138,7 +215,7 @@ class ODataQueryParser:
         options : ODataQueryOptions
             Current query options object.
         """
-        options.filter_ = self.filter_parser.parse(value)
+        options.filter_ = self.__filter_parser.parse(value)
 
     def _parse_format(self, value: str, options: ODataQueryOptions) -> None:
         """Parses $format parameter.
@@ -155,7 +232,7 @@ class ODataQueryParser:
         UnsupportedFormat
             If the format is not supported.
         """
-        if value not in ('json', 'xml', 'csv', 'tsv'):
+        if value not in self.__supported_formats:
             raise UnsupportedFormat(value)
 
         options.format_ = value
@@ -170,24 +247,21 @@ class ODataQueryParser:
         options : ODataQueryOptions
             Current query options object.
         """
-        if not value:
-            return None
-
         orderby_list = []
         for item in value.split(','):
             item = item.strip()
             lowercased_item = item.lower()
             if lowercased_item.endswith(' asc'):
-                field = item.replace(' asc', '').strip()
-                direction = 'asc'
+                field, direction = item.rsplit(' asc', 1)
             elif lowercased_item.endswith(' desc'):
-                field = item.replace(' desc', '').strip()
-                direction = 'desc'
+                field, direction = item.rsplit(' desc', 1)
             else:
                 field = item
                 direction = 'asc'  # default direction
 
-            orderby_list.append({'field': field, 'direction': direction})
+            orderby_list.append(
+                {'field': field.strip(), 'direction': direction.strip()}
+            )
 
         options.orderby = orderby_list
 
@@ -213,8 +287,7 @@ class ODataQueryParser:
         options : ODataQueryOptions
             Current query options object.
         """
-        if value:
-            options.select = [item.strip() for item in value.split(',')]
+        options.select = self._split_by_comma(value)
 
     def _parse_skip(self, value: str, options: ODataQueryOptions) -> None:
         """Parses $skip parameter.
@@ -257,3 +330,18 @@ class ODataQueryParser:
             assert options.top >= 0
         except (AssertionError, ValueError, TypeError):
             raise NoPositiveIntegerValue('$top', value)
+
+    def _split_by_comma(self, value: str) -> list[str]:
+        """Splits a string by comma.
+
+        Parameters
+        ----------
+        value : str
+            String to be split.
+
+        Returns
+        -------
+        list[str]
+            List of strings.
+        """
+        return [item.strip() for item in value.split(',')]
