17b2be742e318f12c97d6a0b00398e353e02968d
Dairo Mosquera <dairoandres115@outlook.com>
2025-03-15 02:34:13 -0500

    tests: add tests core file and first test for beanie utils

---
+++ tests/_core.py
@@ -0,0 +1,36 @@
+from beanie import Document, init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+from pydantic import BaseModel
+
+MONGO_CONN_STR = 'mongodb+srv://test:test@cluster0.527ii.mongodb.net/'
+
+
+class User(Document):
+    name: str
+    email: str
+    age: int
+
+
+class UserProjection(BaseModel):
+    name: str
+    email: str
+
+
+async def get_client():
+    client = AsyncIOMotorClient(MONGO_CONN_STR)
+    await init_beanie(database=client.db_name, document_models=[User])
+    return client
+
+
+async def seed_data():
+    await User.find().delete()
+    await User(name='John', email='john@example.com', age=25).insert()
+    await User(name='Jane', email='jane@example.com', age=30).insert()
+    await User(name='Alice', email='alice@example.com', age=35).insert()
+    await User(name='Bob', email='bob@example.com', age=40).insert()
+    await User(name='Bobby', email='bobby@example.com', age=28).insert()
+    await User(name='Charlie', email='charlie@example.com', age=32).insert()
+    await User(name='David', email='david@example.com', age=41).insert()
+    await User(name='Eve', email='eve@example.com', age=54).insert()
+    await User(name='Frank', email='frank@example.com', age=29).insert()
+    await User(name='Grace', email='grace@example.com', age=37).insert()
